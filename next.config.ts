6dd92eda59c61086be5b78714099f42bb97a74e8
seastj <dev.seastj@gmail.com>
2025-10-20 14:06:22 +0900

    [docs] zustand 설정 및 테스트

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   sassOptions: {
     includePaths: ["./src/styles"]
-  }
+  }, images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fakestoreapi.com",
+      },
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
