1e849a52e6320a281e6a92d769c2c18c3f353590
seastj <dev.seastj@gmail.com>
2025-10-21 11:30:36 +0900

    [docs] zustand 설정 및 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import ButtonTest from '@/components/ButtonTest';
 import Counter from '@/components/Counter';
 import SCSSTest from '@/components/SCSSTest';
+import ThemeToggle from '@/components/ThemeToggle';
+import TodoList from '@/components/TodoList';
 import UserProfile from '@/components/UserProfile';
 
 export default function Home() {
@@ -10,7 +12,14 @@ export default function Home() {
       <SCSSTest />
       <Counter />
       <br />
+      <br />
       <UserProfile />
+      <br />
+      <br />
+      <ThemeToggle />
+      <br />
+      <br />
+      <TodoList />
     </div>
   );
 }
