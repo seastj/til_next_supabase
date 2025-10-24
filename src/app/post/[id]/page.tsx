eee3daee875e16c3c5438f48042e404414fa2fdb
seastj <dev.seastj@gmail.com>
2025-10-24 10:13:05 +0900

    [docs] 라우터 설정

---
+++ src/app/post/[id]/page.tsx
@@ -0,0 +1,11 @@
+interface PostDetailProps {
+  params: { id: string };
+}
+
+function PostDetail({ params }: PostDetailProps) {
+  const { id } = params;
+  
+  return <div>PostDetail</div>;
+}
+
+export default PostDetail;
