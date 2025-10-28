4e6ba5ef28460a3d862b49f0b49f64ab65104ad6
seastj <dev.seastj@gmail.com>
2025-10-28 11:54:47 +0900

    [docs] 카카오 인증

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,4 +1,5 @@
 import supabase from '@/lib/supabase/client';
+import type { Provider } from '@supabase/auth-js';
 
 // Supabase 백엔드에 사용자 이메일 회원 가입
 export async function signUpWithEmail({
@@ -10,6 +11,32 @@ export async function signUpWithEmail({
 }) {
   // 웹브라우저를 이용해서 이메일 회원가입
   const { data, error } = await supabase.auth.signUp({ email, password });
+  if (error) throw error;
+  return data;
+}
+
+// supabase 백엔드에 사용자 이메일 로그인
+export async function signInWithPassword({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) {
+  // 레퍼런스 참조
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+  if (error) throw error;
+
+  return data;
+}
+
+// supabase 백엔드에 소셜 로그인
+export async function signInWithOAuth(provider: Provider) {
+  const { data, error } = await supabase.auth.signInWithOAuth({ provider });
+
   if (error) throw error;
   return data;
 }
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,5 +1,81 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { useSignInWithKakao } from '@/hooks/mutations/useSignInWithKakao';
+import { useSignInWithPassword } from '@/hooks/mutations/useSignInWithPassword';
+import Link from 'next/link';
+import { useState } from 'react';
+
 function SignIn() {
-  return <div>SignIn</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  // 이메일로 로그인
+  const { mutate: signInPassword, isPending: isPendingPassword } =
+    useSignInWithPassword();
+  const handleSignInWithEmail = () => {
+    if (!email.trim()) return;
+    if (!password.trim()) return;
+    // 이메일을 이용해서 로그인 진행
+    signInPassword({ email, password });
+  };
+
+  // 카카오 로그인
+  const { mutate: signInWithKakao, isPending: isPendingKakao } =
+    useSignInWithKakao();
+  const handleSignInWithKakao = () => {
+    signInWithKakao('kakao');
+  };
+
+  return (
+    <div className='flex flex-col gap-8'>
+      <div className='text-xl font-bold'>로그인</div>
+      <div className='flex flex-col gap-2'>
+        <Input
+          type='email'
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          disabled={isPendingPassword}
+          className='py-6'
+          placeholder='example@example.com'
+        />
+        <Input
+          type='password'
+          value={password}
+          disabled={isPendingPassword}
+          onChange={e => setPassword(e.target.value)}
+          className='py-6'
+          placeholder='password'
+        />
+      </div>
+      <div className='flex flex-col gap-2'>
+        {/* 비밀번호 및 이메일 로그인 */}
+        <Button
+          onClick={handleSignInWithEmail}
+          disabled={isPendingPassword}
+          className='w-full cursor-pointer'
+        >
+          로그인
+        </Button>
+        {/* 카카오 소셜 로그인 */}
+        <Button
+          onClick={handleSignInWithKakao}
+          disabled={isPendingKakao}
+          className='w-full cursor-pointer'
+        >
+          카카오 계정 로그인
+        </Button>
+      </div>
+      <div>
+        <Link
+          href={'/signup'}
+          className='text-muted-foreground hover:underline'
+        >
+          계정이 없으시다면? 회원가입
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default SignIn;
