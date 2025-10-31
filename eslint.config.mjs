5c726ace4de28b9a63e7365457aa6b396636ab5a
seastj <dev.seastj@gmail.com>
2025-10-31 11:01:47 +0900

    [docs] zustand 인증 보관 적용

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -24,6 +24,7 @@ const eslintConfig = [
       'prettier/prettier': ['warn', { endOfLine: 'auto' }], //  Prettier 스타일을 강제 적용 (오류 발생 시 ESLint에서 표시)
       '@typescript-eslint/no-unused-vars': 'warn', //  기존 TypeScript 규칙 유지
       '@typescript-eslint/no-explicit-any': 'off', //  any 타입 사용 허용
+      '@typescript-eslint/no-unused-vars' : 'off'
     },
   },
 ];
--- src/components/counter/Viewer.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-import { useCount } from '@/stores/count';
-
-const Viewer = () => {
-  const count = useCount();
-  return <div className='text-4xl font-bold'>{count}</div>;
-};
-
-export default Viewer;
