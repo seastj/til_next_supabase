a0d6a5983bd8b3ec35c4e5ae802c4e0bde24d2e7
seastj <dev.seastj@gmail.com>
2025-11-07 11:52:55 +0900

    [docs] 데이터 정규화

--- src/hooks/todos/mutations/useUpdateTodoMutation.ts
+++ src/hooks/todos/mutations/useUpdateTodoMutation.ts
@@ -1,49 +1,40 @@
 import { updateTodo } from '@/apis/todo';
 import { QUERY_KEYS } from '@/lib/constants';
-import { queryClinet } from '@/lib/query-client';
 import { Todo } from '@/types/todo-type';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 export function useUpdateTodoMutation() {
-  // 1. 전역 접근용 변수
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: updateTodo,
-    // mutate 함수가 실행되는 시점에 작성
     onMutate: async updatedTodo => {
-      // 요청 취소 기능 구현
-      await queryClient.cancelQueries({ queryKey: QUERY_KEYS.todo.list });
-
-      // 원본 데이터를 보관함
-      const originTodos = queryClinet.getQueryData<Todo[]>(
-        QUERY_KEYS.todo.list
-      );
-      // 낙관적 업데이트 진행함
-      queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, prevTodos => {
-        if (!prevTodos) return [];
-        return prevTodos.map(todo =>
-          todo.id === updatedTodo.id ? { ...todo, ...updatedTodo } : todo
-        );
+      // 하나만 업데이트 하기
+      await queryClient.cancelQueries({
+        queryKey: QUERY_KEYS.todo.detail(updatedTodo.id.toString()),
       });
+      const prevTodo = queryClient.getQueryData<Todo>(
+        QUERY_KEYS.todo.detail(updatedTodo.id.toString())
+      );
+      // 업데이트 된 데이터만 반영해줌
+      queryClient.setQueryData<Todo>(
+        QUERY_KEYS.todo.detail(updatedTodo.id.toString()),
+        prevTodo => {
+          if (!prevTodo) return;
+          return { ...prevTodo, ...updatedTodo };
+        }
+      );
 
-      // 원상 복구할 데이터를 리턴해 준다.
-      return { originTodos };
+      return { prevTodo };
     },
     // 에러가 발생함
     onError: (error, valiable, context) => {
-      console.log(error);
-      if (context?.originTodos) {
-        queryClient.setQueryData<Todo[]>(
-          QUERY_KEYS.todo.list,
-          context.originTodos
+      if (context?.prevTodo) {
+        queryClient.setQueryData<Todo>(
+          QUERY_KEYS.todo.detail(context.prevTodo.id.toString()),
+          context.prevTodo
         );
       }
       return { error };
-    },
-    // 요청이 완료됨
-    onSettled: () => {
-      // 검증 과정
-      queryClient.invalidateQueries({ queryKey: QUERY_KEYS.todo.list });
     },
   });
 }
