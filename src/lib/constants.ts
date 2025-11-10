21c09be1011f7203297fb62ca83d02cf717ee4e7
seastj <dev.seastj@gmail.com>
2025-11-10 11:11:02 +0900

    [docs] 사용자 프로필 추가

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,4 +5,11 @@ export const QUERY_KEYS = {
     list: ['todos', 'list'],
     detail: (id: string) => ['todos', 'deatail', id],
   },
+
+  // 프로필 useQuery 키 생성 및 관리
+  profile: {
+    all: ['profile'],
+    list: ['profile', 'list'],
+    byId: (userId: string) => ['profile', 'byId', userId],
+  },
 };
