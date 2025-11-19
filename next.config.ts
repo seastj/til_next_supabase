91e1fb8a3a99d9bf30b8ef7359d613852f89615a
seastj <dev.seastj@gmail.com>
2025-11-19 11:44:30 +0900

    [docs] 프로필 수정 적용

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns:[{
+      protocol: "https",
+      hostname: "nowpnwclkahiucqlchfw.supabase.co",
+      // 아래는 생략 가능
+      pathname:"/storage/v1/object/public/**"
+    }]
+  }
 };
 
 export default nextConfig;
